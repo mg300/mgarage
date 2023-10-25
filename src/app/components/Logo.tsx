52125e4fba2fe48358d640bd5b401f06b24fd63b
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-10-25 13:44:16 +0200

    fix: fix styles

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Logo() {
   return (
-    <div className="font-logo">
-      <span className="text-logo text-6xl font-bold tracking-tight">MG</span>
-      <span className="text-xl font-normal">arage</span>
+    <div className="font-logo cursor-pointer group">
+      <span className="text-logo text-6xl font-bold group-hover:text-red-600 tracking-tight">MG</span>
+      <span className="text-xl group-hover:text-gray-700  font-normal">arage</span>
     </div>
   );
 }
