cbfa6b6c58f0f553797a74e7f8809b96dfeb7290
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-10-25 15:51:04 +0200

    feta: add button

---
+++ src/app/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Button({ children }: { children: React.ReactNode }) {
+  return (
+    <button
+      type="button"
+      className="text-white bg-gradient-to-r from-logo to-red-900 hover:bg-gradient-to-tl font-semibold rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 font-sans"
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
