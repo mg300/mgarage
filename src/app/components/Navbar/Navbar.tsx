a05d54438a1682299b0d4aadee8bc571285a42de
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-10-25 13:59:50 +0200

    feat: add navbar

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -6,9 +6,11 @@ function Navbar() {
     <div className="flex justify-between bg-none m-5 items-center">
       <Logo />
       <div>
-        <button className="mr-10 cursor-pointer hover:text-gray-700  font-semibold ">Home</button>
-        <button className="mr-10 cursor-pointer hover:text-gray-700  font-semibold ">Service</button>
-        <button className="mr-10 cursor-pointer hover:text-gray-700  font-semibold ">Contact us</button>
+        <button className="mr-10 hidden md:inline cursor-pointer hover:text-gray-700  font-semibold ">Home</button>
+        <button className="mr-10 hidden md:inline cursor-pointer hover:text-gray-700  font-semibold ">Service</button>
+        <button className="mr-10 hidden md:inline cursor-pointer hover:text-gray-700  font-semibold ">
+          Contact us
+        </button>
         <button className="mr-10 cursor-pointer hover:text-gray-700  font-bold border-collapse text-logo">
           Umów wizytę
         </button>
