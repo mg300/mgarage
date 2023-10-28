7c66636a6f739fe2921b8ccfe4fdd69ef6a1fa55
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-10-28 16:04:29 +0200

    feat: implement rwd

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,33 +3,35 @@ import Image from "next/image";
 import Logo from "../Logo";
 function Footer() {
   return (
-    <div className="bg-black h-96 w-full min-w-[1080px] px-20 pt-10 justify-between items-start inline-flex flex-wrap">
-      <Logo color="text-white" />
-      <div className="text-white  font-body  flex-col">
+    <div className="bg-black h-max lg:h-96 w-full px-20 pt-10 justify-between items-start flex flex-wrap flex-col lg:flex-row">
+      <div className="self-center lg:self-start text-center lg:text-left mb-10 lg:mb-0">
+        <Logo color=" text-white" />
+      </div>
+      <div className="self-center lg:self-start mb-10 lg:mb-0 text-center lg:text-left text-white font-body flex-col">
         <p className="text-3xl font-bold leading-[68px]">Lokalizacja</p>
         <p className="text-xl leading-8 text-gray-400">
           Warsztatowa 123<br></br>
           12-345 Warszawa<br></br> +48 123 456 789<br></br>mateuszjacekgajda@gmail.com
         </p>
       </div>
-      <div className="text-white  font-body  flex-col">
+      <div className="self-center lg:self-start mb-10 lg:mb-0 text-center lg:text-left text-white font-body flex-col">
         <p className="text-3xl font-bold leading-[68px]">Godziny otwarcia</p>
         <p className="text-xl leading-8 text-gray-400">
           Poniedziałek-Piątek: 8:00 - 18:00<br></br>Sobota: 9:00 - 14:00<br></br>Niedziela: Zamknięte<br></br>
         </p>
       </div>{" "}
-      <div className="text-white  font-body  flex-col">
+      <div className="self-center lg:self-start mb-10 lg:mb-0 text-center lg:text-left text-white font-body flex-col">
         <p className="text-3xl font-bold leading-[68px] hover:font-bold">Sprawdź</p>
-        <a className="text-xl leading-8 text-gray-400  hover:text-logo" href="https://facebook.com">
+        <a className="text-xl leading-8 text-gray-400 hover:text-logo" href="https://facebook.com">
           Facebook
         </a>
         <br></br>
-        <a className="text-xl leading-8 text-gray-400  hover:text-logo" href="https://facebook.com">
+        <a className="text-xl leading-8 text-gray-400 hover:text-logo" href="https://facebook.com">
           Instagram
         </a>
       </div>
-      <div className="basis-full"></div>
-      <div className="text-white text-xs"> Copyright © 2023 Mateusz Gajda. All Rights Reserved</div>
+      <div className="lg:basis-full basis-0"></div>
+      <div className="text-white text-xs mb-5"> Copyright © 2023 Mateusz Gajda. All Rights Reserved</div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import BackgroundMain from "./components/BackgroundMain";
 export default function Home() {
   return (
     <div className="h-screen min-h-[900px] w-[35rem]">
-      <p className="ml-10 leading-[60px] pt-64 text-gray-200 text-5xl font-extrabold ">
+      <p className="ml-10 leading-[60px] pt-64 text-gray-200 md:text-5xl text-4xl font-extrabold ">
         PARTNER TWOJEGO<br></br> SAMOCHODU
       </p>
-      <p className="ml-10 text-black font-bold text-xl">Sprawdź dostępność i zarezerwuj termin naprawy online!</p>
+      <p className="ml-10 text-black font-bold md:text-xl text-base">
+        Sprawdź dostępność i zarezerwuj termin naprawy online!
+      </p>
       <div className="mt-8 ml-10">
         <Button>Zarezerwuj termin</Button>
       </div>
