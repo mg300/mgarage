70c7aef6e9f40abbb3087ac9573995fb35630c94
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-10-28 17:35:48 +0200

    feat: add about section with rwd

---
+++ src/app/components/About.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+function About() {
+  return (
+    <div className="flex flex-col items-center bg-[#373737]">
+      <div className="text-gray-200 lg:text-5xl text-5xl font-extrabold my-10">O NAS</div>
+      <div className="flex flex-col lg:flex-row items-center lg:mx-10 gap-[100px]">
+        <div className="w-[90%] lg:w-[70%] text-justify flex:1 text-gray-200 font-medium lg:leading-8 xl:leading-10">
+          Naszą misją jest dostarczanie usług najwyższej jakości dla naszych klientów, zarówno indywidualnych, jak i
+          firm. Nasz doświadczony zespół śledzi najnowsze trendy w motoryzacji i stawia na transparentność oraz dbałość
+          o szczegóły. Dla nas motoryzacja to nie tylko praca, to nasza pasja i styl życia. Nie tylko naprawiamy
+          usterki, ale również oferujemy regularne przeglądy i konserwację, które pomagają utrzymać Twój samochód w
+          doskonałej kondycji. Nasze zaangażowanie w środowisko skłania nas także do stosowania ekologicznych rozwiązań,
+          gdy to możliwe.
+        </div>
+        <div className="lg:w-[50%] max-w-[700px]">
+          <Image
+            className="w-full my-10"
+            alt="car"
+            src="/about_car.png"
+            width={600}
+            height={600}
+            loading="lazy"
+          ></Image>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
-import Image from "next/image";
 import Button from "./components/Button/Button";
 import BackgroundMain from "./components/BackgroundMain";
+import About from "./components/About";
 
 export default function Home() {
   return (
-    <div className="h-screen min-h-[900px] w-[35rem]">
-      <p className="ml-10 leading-[60px] pt-64 text-gray-200 md:text-5xl text-4xl font-extrabold ">
-        PARTNER TWOJEGO<br></br> SAMOCHODU
-      </p>
-      <p className="ml-10 text-black font-bold md:text-xl text-base">
-        Sprawdź dostępność i zarezerwuj termin naprawy online!
-      </p>
-      <div className="mt-8 ml-10">
-        <Button>Zarezerwuj termin</Button>
+    <>
+      <div className="h-screen min-h-[900px] w-[35rem]">
+        <p className="ml-10 leading-[60px] pt-64 text-gray-200 md:text-5xl text-4xl font-extrabold ">
+          PARTNER TWOJEGO<br></br> SAMOCHODU
+        </p>
+        <p className="ml-10 text-black font-bold md:text-xl text-base">
+          Sprawdź dostępność i zarezerwuj termin naprawy online!
+        </p>
+        <div className="mt-8 ml-10">
+          <Button>Zarezerwuj termin</Button>
+        </div>
+        <BackgroundMain />
       </div>
-      <BackgroundMain />
-    </div>
+      <About />
+    </>
   );
 }
