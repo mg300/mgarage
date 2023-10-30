b7a6d616c7ffe8d147d8950354654be9900ab168
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-10-31 00:12:20 +0100

    feat: add tool

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -1,38 +1,55 @@
 import React from "react";
 import Image from "next/image";
-
+import { FiTool } from "react-icons/fi";
 function Services() {
   return (
-    <div className="flex flex-col items-center bg-[#252525]">
+    <div className="font-body flex flex-col items-center bg-[#252525]">
       <div className="text-gray-200 lg:text-5xl text-5xl font-extrabold my-10">Nasze usługi</div>
       <div className="flex flex-col lg:flex-row items-center lg:mx-10 lg:mb-10 gap-[30px]">
-        <div className="lg:w-[50%] max-w-[700px]">
-          <Image src="/engine2.jpg" width={400} height={400} alt="engine" />
+        <div className="min-w-[400px] max-w-[700px]">
+          <Image src="/engine2.jpg" width={500} height={500} alt="engine" />
         </div>
-        <div className="w-[90%] lg:w-[70%] text-justify flex:1 text-gray-200 font-medium lg:leading-8 xl:leading-10">
-          <ul className="leading-8 text-lg">
+        <div className="w-[90%] lg:w-[80%] text-justify flex:1 text-gray-200 font-medium lg:leading-8 xl:leading-10">
+          <ul className="leading-8 text-lg inline-block">
             <li>
+              <span className="text-3xl inline-block mr-5">
+                <FiTool />
+              </span>
               <span className="font-bold">Naprawy eksploatacyjne:</span> hamulce, olej, komplet filtrów
             </li>
             <br></br>
             <li>
+              <span className="text-3xl inline-block mr-5">
+                <FiTool />
+              </span>
               <span className="font-bold">Pielęgnacja:</span> czyszczenie wnętrza, odkurzanie, konserwacja{" "}
             </li>
             <br></br>
             <li>
-              <span className="font-bold">Naprawy mechaniczne:</span> rozrządy, uszczelki pod głowicą, turbosprężarki,
-              alternatory
+              <span className="text-3xl inline-block mr-5">
+                <FiTool />
+              </span>
+              <span className="font-bold">Naprawy mechaniczne:</span> rozrządy, turbosprężarki, alternatory
             </li>
             <br></br>
             <li>
+              <span className="text-3xl inline-block mr-5">
+                <FiTool />
+              </span>
               <span className="font-bold">Naprawy zawieszenia:</span> tuleje, wahacze, amoryzatory, sprężyny
             </li>
             <br></br>
             <li>
+              <span className="text-3xl inline-block mr-5">
+                <FiTool />
+              </span>
               <span className="font-bold">Naprawy elektryczne:</span> przerwane przewody, problemy elektryczne
             </li>
             <br></br>
             <li>
+              <span className="text-3xl inline-block mr-5">
+                <FiTool />
+              </span>
               <span className="font-bold">Diagnostyka:</span> zawieszenia, silnika, hamulców, układu ABS
             </li>
           </ul>
