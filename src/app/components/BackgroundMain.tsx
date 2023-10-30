93962b0f7dc16387fdfa2d04554c7b9df15c8b6c
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-10-30 23:57:21 +0100

    fix: styling

--- src/app/components/BackgroundMain.tsx
+++ src/app/components/BackgroundMain.tsx
@@ -4,7 +4,7 @@ function BackgroundMain() {
   return (
     <div
       className="absolute w-screen -z-10 top-0 h-screen min-h-[900px] bg-cover bg-right xl:bg-auto"
-      style={{ backgroundImage: `url('/background.png')` }}
+      style={{ backgroundImage: `url('/background.png')`, backgroundRepeat: "no-repeat" }}
     >
       <div className="absolute w-screen h-screen bg-gradient-to-r min-h-[900px] from-black to-transparent opacity-50 z-0"></div>
     </div>
