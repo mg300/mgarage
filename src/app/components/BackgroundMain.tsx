fe73722491620472b4b00f045e2f1a9fe86a40fb
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-11-07 16:33:01 +0100

    fix: fix backround on big screens

--- src/app/components/BackgroundMain.tsx
+++ src/app/components/BackgroundMain.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function BackgroundMain() {
   return (
     <div
-      className="absolute w-screen -z-10 top-0 h-screen min-h-[900px] bg-cover bg-right xl:bg-auto"
+      className="absolute w-screen -z-10 top-0 h-screen min-h-[900px] bg-cover bg-right "
       style={{ backgroundImage: `url('/background.png')`, backgroundRepeat: "no-repeat" }}
     >
       <div className="absolute w-screen h-screen bg-gradient-to-r min-h-[900px] from-black to-transparent opacity-50 z-0"></div>
