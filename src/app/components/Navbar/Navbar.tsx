3162daec425a1d7ce9cfaeca6d8e876e581fc4fb
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-11-07 17:01:57 +0100

    feat: change navbar to book view

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,25 +1,43 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 function Navbar() {
+  const pathname = usePathname();
   return (
     <div className="absolute top-0 w-full flex justify-between bg-none h-24 px-6 items-center">
-      <div className="mt-4 cursor-pointer ">
+      <Link href="/" className="mt-4 cursor-pointer ">
         <Image alt="abc" src="/darkLogo.png" width={220} height={150} />
-      </div>
+      </Link>
+
       <div>
-        <button className="mr-10 hidden text-lg md:inline cursor-pointer hover:text-gray-700  font-semibold ">
+        <button
+          className={`mr-10 ${
+            pathname === "/" ? "hidden" : ""
+          } text-lg md:inline cursor-pointer hover:text-gray-700  font-semibold `}
+        >
           O nas
         </button>
-        <button className="mr-10 text-lg hidden md:inline cursor-pointer hover:text-gray-700  font-semibold ">
+        <button
+          className={`mr-10 text-lg ${
+            pathname === "/" ? "hidden" : ""
+          } md:inline cursor-pointer hover:text-gray-700  font-semibold `}
+        >
           Usługi
         </button>
-        <button className="mr-10  text-lg hidden md:inline cursor-pointer hover:text-gray-700  font-semibold ">
+        <button
+          className={`mr-10  text-lg hidden
+          md:inline cursor-pointer hover:text-gray-700  font-semibold `}
+        >
           Skontaktuj się
         </button>
-        <button className="lg:mr-10 text-lg cursor-pointer hover:text-gray-700  font-bold border-collapse text-logo">
-          Umów wizytę
-        </button>
+        {pathname === "/" && (
+          <button className="lg:mr-10 text-lg cursor-pointer hover:text-gray-700  font-bold border-collapse text-logo">
+            Umów wizytę
+          </button>
+        )}
       </div>
     </div>
   );
