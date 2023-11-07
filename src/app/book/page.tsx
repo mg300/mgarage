acbab8c5feb98597bbf08f18a67dfce52899b015
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-11-07 20:48:42 +0100

    feat: add changing content

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -5,10 +5,11 @@ function page() {
   const [clicked, setClicked] = useState("");
   const [selected, setSelected] = useState<string[]>([]);
   const handleClick = function (arg: string) {
-    const result = selected.find((value) => value === "oil");
+    const result = selected.find((value) => value === arg);
     if (clicked == arg) setClicked("");
-    else setClicked(arg);
+
     if (!result) {
+      setClicked(arg);
       setSelected([...selected, arg]);
     }
     if (result) {
@@ -17,31 +18,30 @@ function page() {
     }
   };
   return (
-    <div className="h-screen pt-32 font-body mx-auto max-w-[80rem] ">
+    <div className="h-screen pt-32 font-body px-5 mx-auto max-w-[80rem] ">
       <p className="text-logo font-semibold text-2xl mb-10">Dostępne usługi:</p>
-      <div className="flex gap-14">
+      <div className="flex flex-col lg:flex-row gap-14">
         <div className="flex flex-col gap-5">
           <button
             onClick={() => handleClick("oil")}
-            className={`flex justify-between items-center xl:w-[30rem] h-[6rem] px-10 border-2 border-solid  ${
-              clicked === "oil" ? "border-logo " : "border-gray-300"
-            } rounded-[2rem] cursor-pointer`}
+            className={`flex justify-between items-center xl:w-[30rem] h-[6rem] px-10 border-2 border-solid  
+            ${selected.includes("oil") ? "border-logo " : "border-gray-300"} rounded-[2rem] cursor-pointer `}
           >
-            <p className=" text-lg text-gray-600">Wymiana oleju silnikowego</p>
+            <p className=" text-sm lg:text-lg text-gray-600">Wymiana oleju silnikowego</p>
             <div>
-              <p className=" text-lg text-gray-600">500,00 zł</p>
+              <p className=" text-sm lg:text-lg text-gray-600">od 500,00 zł</p>
               <p className=" text-sm text-gray-600">1h</p>
             </div>
           </button>
           <button
             onClick={() => handleClick("fuel")}
             className={`flex justify-between items-center xl:w-[30rem] h-[6rem] px-10 border-2 border-solid  ${
-              clicked === "fuel" ? "border-logo " : "border-gray-300"
-            } rounded-[2rem] cursor-pointer`}
+              selected.includes("fuel") ? "border-logo " : "border-gray-300"
+            } rounded-[2rem] cursor-pointer `}
           >
-            <p className=" text-lg text-gray-600">Wymiana oleju silnikowego</p>
+            <p className=" text-sm lg:text-lg text-gray-600">Wymiana oleju silnikowego</p>
             <div>
-              <p className=" text-lg text-gray-600">500,00 zł</p>
+              <p className=" text-sm lg:text-lg text-gray-600">od 500,00 zł</p>
               <p className=" text-sm text-gray-600">1h</p>
             </div>
           </button>
