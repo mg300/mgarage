24954b060318aff38259c96d527c6cabc45f8280
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-11-09 15:46:19 +0100

    fix: background width

--- src/app/components/BackgroundMain.tsx
+++ src/app/components/BackgroundMain.tsx
@@ -3,10 +3,10 @@ import React from "react";
 function BackgroundMain() {
   return (
     <div
-      className="absolute w-screen -z-10 top-0 h-screen min-h-[900px] bg-cover bg-right "
+      className="absolute w-full -z-10 top-0 h-screen min-h-[900px] bg-cover bg-right "
       style={{ backgroundImage: `url('/background.png')`, backgroundRepeat: "no-repeat" }}
     >
-      <div className="absolute w-screen h-screen bg-gradient-to-r min-h-[900px] from-black to-transparent opacity-50 z-0"></div>
+      <div className="absolute w-full h-screen bg-gradient-to-r min-h-[900px] from-black to-transparent opacity-50 z-0"></div>
     </div>
   );
 }
