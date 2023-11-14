3c318b9390fdec02a0ce045f15f31dbb239dcd25
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-11-14 11:55:24 +0100

    feat: add onClick prop

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -4,7 +4,9 @@ function Button({
   children,
   color,
   type = "button",
+  onClick,
 }: {
+  onClick?: () => void;
   children: React.ReactNode;
   color: string;
   type?: "button" | "submit" | "reset" | undefined;
@@ -12,6 +14,7 @@ function Button({
   return (
     <button
       type={type}
+      onClick={onClick}
       className={`text-white bg-gradient-to-r ${
         color === "red" ? " from-logo to-red-900" : "from-blue-600 to-blue-900"
       } hover:bg-gradient-to-tl font-semibold rounded-lg text-base px-5 py-2.5 text-center mr-2 mb-2 font-sans focus:ring-4`}
