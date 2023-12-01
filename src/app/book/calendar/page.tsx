3be471f1e287245edec43bd7667664ee83d8a8ab
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2023-12-01 18:44:12 +0100

    fix: bug with month length

--- src/app/book/calendar/page.tsx
+++ src/app/book/calendar/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 interface day {
   number: number;
@@ -13,6 +13,7 @@ interface month {
 }
 
 function Page() {
+  // const currentDate = useMemo(() => new Date(), []);
   const [month, setMonth] = useState<month>({
     days: [{ number: 1, hours: [1, 2, 3] }],
     length: 1,
@@ -21,9 +22,10 @@ function Page() {
   });
   useEffect(() => {
     const currentDate = new Date();
-    const firstDayOfMonthNum = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay();
-
-    const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+    currentDate.setMonth(currentDate.getMonth());
+    const firstDayOfMonthNum = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay() - 1;
+    const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+    console.log(currentDate.getMonth(), currentDate.getDate(), daysInMonth);
     const tempMonth = [];
     for (let i = 1; i <= daysInMonth; i++) {
       tempMonth.push({ number: i, hours: [] });
@@ -39,36 +41,40 @@ function Page() {
     <div className="min-h-screen pt-40 font-body  mx-auto max-w-[80rem] ">
       <p className="text-logo font-semibold text-2xl mb-10">Wprowadź datę:</p>
       <div className="relative p-16 border-2 border-solid border-gray-200 shadow-lg min-h-[40rem] mb-20">
-        <form className="grid grid-cols-7 w-[50%] gap-2 justify-items-center font-semibold">
-          <span>Pn</span>
-          <span>Wt</span>
-          <span>Śr</span>
-          <span>Czw</span>
-          <span>Pt</span>
-          <span>Sob</span>
-          <span>Nd</span>
-          {[...Array(month.firstDayOfMonthNum - 1)].map((_, index) => (
-            <div key={index}></div>
-          ))}
-          {month.days.map((day, index) => (
-            <div key={index}>
-              <input
-                className="hidden peer"
-                id={day.number.toString()}
-                name="day"
-                type="radio"
-                disabled={index < month.currentDay}
-              />
-              <label
-                className={`block cursor-pointer text-center peer-checked:text-red-800 w-[3rem] h-[3rem] peer-checked:border-2 p-3 peer-checked:border-red-500 peer-checked:rounded-xl font-bold ${
-                  index < month.currentDay ? "text-gray-400 " : "text-gray-700 "
-                }`}
-                htmlFor={day.number.toString()}
-              >
-                {day.number}
-              </label>
-            </div>
-          ))}
+        <form className="flex">
+          <button>prev</button>
+          <div className="grid grid-cols-7 w-[50%] gap-2 justify-items-center font-semibold">
+            <span>Pn</span>
+            <span>Wt</span>
+            <span>Śr</span>
+            <span>Czw</span>
+            <span>Pt</span>
+            <span>Sob</span>
+            <span>Nd</span>
+            {[...Array(month.firstDayOfMonthNum)].map((_, index) => (
+              <div key={index}></div>
+            ))}
+            {month.days.map((day, index) => (
+              <div key={index}>
+                <input
+                  className="hidden peer"
+                  id={day.number.toString()}
+                  name="day"
+                  type="radio"
+                  disabled={index < month.currentDay}
+                />
+                <label
+                  className={`block cursor-pointer text-center peer-checked:text-red-800 w-[3rem] h-[3rem] peer-checked:border-2 p-3 peer-checked:border-red-500 peer-checked:rounded-xl font-bold ${
+                    index < month.currentDay ? "text-gray-400 " : "text-gray-700 "
+                  }`}
+                  htmlFor={day.number.toString()}
+                >
+                  {day.number}
+                </label>
+              </div>
+            ))}
+          </div>
+          <button>next</button>
         </form>
       </div>
     </div>
