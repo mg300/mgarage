4a4ba64004fdcfb7fdbbee850f5bce4e7e381cb1
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-01-24 14:06:23 +0100

    add enpoint find by id

---
+++ src/app/api/services/[id]/route.tsx
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(request: NextResponse, { params }: any) {
+  const id = parseInt(params.id);
+  try {
+    const data = await prisma.services.findFirst({ where: { id } });
+    return NextResponse.json([data]);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
