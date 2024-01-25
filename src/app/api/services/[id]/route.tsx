83faa21a0fbd6d323dfea7d240d540616570a528
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-01-25 11:32:33 +0100

    change params

--- src/app/api/services/[id]/route.tsx
+++ src/app/api/services/[id]/route.tsx
@@ -4,10 +4,11 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET(request: NextResponse, { params }: any) {
-  const id = parseInt(params.id);
+  const ids = params.id.split(",").map((id: any) => parseInt(id));
+  console.log(ids);
   try {
-    const data = await prisma.services.findFirst({ where: { id } });
-    return NextResponse.json([data]);
+    const data = await prisma.services.findMany({ where: { id: { in: ids } } });
+    return NextResponse.json(data);
   } catch (error) {
     console.error("Error fetching data:", error);
     return new NextResponse("Internal Server Error", { status: 500 });
