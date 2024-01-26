faf0fe8266f417fe90836ede69905b7b7f774a2a
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-01-26 17:59:42 +0100

    add POST endpoint

--- src/app/api/services/route.tsx
+++ src/app/api/services/route.tsx
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -12,5 +12,26 @@ export async function GET() {
     return new NextResponse("Internal Server Error", { status: 500 });
   } finally {
     await prisma.$disconnect();
+  }
+}
+export async function POST(req: NextRequest, res: NextResponse) {
+  if (req.method === "POST") {
+    try {
+      const orderData = await req.json();
+      console.log(orderData);
+      orderData.date = new Date(orderData.date);
+      const order = await prisma.orders.create({
+        data: orderData,
+      });
+
+      return new NextResponse(JSON.stringify(orderData), { status: 201 });
+    } catch (error) {
+      console.error("Error:", error);
+      return new NextResponse("Internal Server Error", { status: 500 });
+    } finally {
+      await prisma.$disconnect();
+    }
+  } else {
+    return new NextResponse("Method Not Allowed", { status: 405 });
   }
 }
